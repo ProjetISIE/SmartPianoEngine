43a9e6cc1126b791fd7aa9e607639b6471d2f6ca
Guilhem Fauré <pro@guilhemfau.re>
2026-02-16 23:01:00 +0100

    build: add nix coverage test

--- flake.nix
+++ flake.nix
@@ -40,6 +40,59 @@
           cross = cross-smart-piano;
         }
       );
+      checks = systems (
+        pkgs: _:
+        let
+          smart-piano = self.packages.${pkgs.stdenv.hostPlatform.system}.smart-piano;
+        in
+        {
+          coverage = pkgs.clangStdenv.mkDerivation rec {
+            pname = "coverage-check";
+            src = self;
+            nativeBuildInputs =
+              (smart-piano.nativeBuildInputs or [ ])
+              ++ (with pkgs; [
+                lcov
+                cmake
+              ]);
+            buildInputs = smart-piano.buildInputs or [ ];
+            cmakeFlags = [
+              "-DCMAKE_BUILD_TYPE=Debug"
+              "-DCOVERAGE=ON"
+            ];
+            configurePhase = ''
+              runHook preConfigure
+              cmake -S . -B build ${toString cmakeFlags}
+              runHook postConfigure
+            '';
+            buildPhase = ''
+              runHook preBuild
+              cmake --build build -j''${NIX_BUILD_CORES:-1}
+              runHook postBuild
+            '';
+            checkPhase = ''
+              runHook preCheck
+              cd build
+              ctest --output-on-failure
+              runHook postCheck
+            '';
+            installPhase = ''
+              runHook preInstall
+              mkdir -p $out/
+              cd build
+              lcov --capture --directory . --output-file coverage.info
+              lcov --remove coverage.info '/usr/*' --output-file coverage.info.filtered
+              lcov --remove coverage.info.filtered '*/test/*' --output-file coverage.info.filtered
+              lcov --remove coverage.info.filtered '*/build/_deps/*' --output-file coverage.info.filtered
+              genhtml coverage.info.filtered --output-directory $out
+              echo "Coverage report generated in $out"
+              lcov --summary coverage.info.filtered
+              runHook postInstall
+            '';
+            meta.description = "Code coverage report for Smart Piano Engine";
+          };
+        }
+      );
       devShells = systems (
         pkgs: crossPkgs: {
           default =
