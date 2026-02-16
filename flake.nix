8a86b223a7bbde1bf8838bcfbcaefbef2f1c94aa
Guilhem Fauré <pro@guilhemfau.re>
2026-02-16 23:32:52 +0100

    build: fix nix coverage test

--- flake.nix
+++ flake.nix
@@ -4,14 +4,16 @@
   outputs =
     { self, nixpkgs }:
     let
-      systems =
+      localSystems = [
+        "aarch64-linux"
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+      forAllSystems =
+        f: nixpkgs.lib.genAttrs localSystems (system: f (import nixpkgs { inherit system; }));
+      forAllSystemsWithCross =
         f:
         let
-          localSystems = [
-            "aarch64-linux"
-            "x86_64-linux"
-            "aarch64-darwin"
-          ];
           crossSystem = "aarch64-linux";
         in
         nixpkgs.lib.genAttrs localSystems (
@@ -25,106 +27,98 @@
         );
     in
     {
-      packages = systems (
-        pkgs: crossPkgs: rec {
+      packages = forAllSystems (
+        pkgs:
+        let
           smart-piano = pkgs.callPackage ./engine.nix {
             inherit self pkgs;
             stdenv = pkgs.clangStdenv;
           };
+        in
+        {
+          inherit smart-piano;
+          default = smart-piano;
+        }
+      );
+      crossPackages = forAllSystemsWithCross (
+        pkgs: crossPkgs:
+        let
           cross-smart-piano = crossPkgs.callPackage ./engine.nix {
             inherit self;
             stdenv = crossPkgs.clangStdenv;
             pkgs = crossPkgs;
           };
-          default = smart-piano;
-          cross = cross-smart-piano;
-        }
-      );
-      checks = systems (
-        pkgs: _:
-        let
-          smart-piano = self.packages.${pkgs.stdenv.hostPlatform.system}.smart-piano;
         in
         {
-          coverage = pkgs.clangStdenv.mkDerivation rec {
-            pname = "coverage-check";
-            src = self;
-            nativeBuildInputs =
-              (smart-piano.nativeBuildInputs or [ ])
-              ++ (with pkgs; [
-                lcov
-                cmake
-              ]);
-            buildInputs = smart-piano.buildInputs or [ ];
-            cmakeFlags = [
-              "-DCMAKE_BUILD_TYPE=Debug"
-              "-DCOVERAGE=ON"
-            ];
-            configurePhase = ''
-              runHook preConfigure
-              cmake -S . -B build ${toString cmakeFlags}
-              runHook postConfigure
-            '';
-            buildPhase = ''
-              runHook preBuild
-              cmake --build build -j''${NIX_BUILD_CORES:-1}
-              runHook postBuild
-            '';
-            checkPhase = ''
-              runHook preCheck
-              cd build
-              ctest --output-on-failure
-              runHook postCheck
-            '';
-            installPhase = ''
-              runHook preInstall
-              mkdir -p $out/
-              cd build
-              lcov --capture --directory . --output-file coverage.info
-              lcov --remove coverage.info '/usr/*' --output-file coverage.info.filtered
-              lcov --remove coverage.info.filtered '*/test/*' --output-file coverage.info.filtered
-              lcov --remove coverage.info.filtered '*/build/_deps/*' --output-file coverage.info.filtered
-              genhtml coverage.info.filtered --output-directory $out
-              echo "Coverage report generated in $out"
-              lcov --summary coverage.info.filtered
-              runHook postInstall
-            '';
-            meta.description = "Code coverage report for Smart Piano Engine";
-          };
+          inherit cross-smart-piano;
+          default = cross-smart-piano;
         }
       );
-      devShells = systems (
-        pkgs: crossPkgs: {
-          default =
-            pkgs.mkShell.override
-              {
-                stdenv = pkgs.clangStdenv; # Clang instead of GCC
-              }
-              {
-                packages = with pkgs; [
-                  bashInteractive
-                  clang-tools # Clang CLIs, including LSP
-                  clang-uml # UML diagram generator
-                  cmake-format # CMake formatter
-                  cmake-language-server # Cmake LSP
-                  # cppcheck # C++ Static analysis
-                  doxygen # Documentation generator
-                  # fluidsynth # JACK Synthesizer
-                  lldb # Clang debug adapter
-                  # neocmakelsp # CMake LSP
-                  # qsynth # FluidSynth GUI
-                  socat # Serial terminal for manual testing
-                  # valgrind # Debugging and profiling
-                ];
-                nativeBuildInputs = self.packages.${pkgs.stdenv.hostPlatform.system}.smart-piano.nativeBuildInputs;
-                buildInputs = self.packages.${pkgs.stdenv.hostPlatform.system}.smart-piano.buildInputs;
-                # Export compile commands JSON for LSP and other tools
-                shellHook = ''
-                  mkdir --verbose build
-                  cmake -DCMAKE_BUILD_TYPE=Debug -DCOVERAGE=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -S . -B build
-                '';
-              };
-        }
-      );
+      checks = forAllSystems (pkgs: {
+        coverage = pkgs.clangStdenv.mkDerivation {
+          name = "coverage-check";
+          src = self;
+          nativeBuildInputs =
+            (self.packages.${pkgs.stdenv.hostPlatform.system}.smart-piano.nativeBuildInputs or [ ])
+            ++ [ pkgs.lcov ];
+          buildInputs = (self.packages.${pkgs.stdenv.hostPlatform.system}.smart-piano.buildInputs or [ ]);
+          configurePhase = ''
+            cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug -DCOVERAGE=ON
+          '';
+          buildPhase = ''
+            cmake --build build
+          '';
+          checkPhase = ''
+            cmake --build build --target tests
+            cmake --build build --target coverage
+            llvm-cov report build/src/main -instr-profile=build/coverage.profdata -ignore-filename-regex='test/.*' > build/coverage.txt
+            cat build/coverage.txt
+            echo "Verifying functions coverage is > 90%"
+            grep TOTAL build/coverage.txt | awk '{ if ($7 + 0 > 90) exit 0; else { print "Function coverage is " $7 "% (< 90%)"; exit 1 } }'
+            echo "Verifying line coverage is > 90%"
+            grep TOTAL build/coverage.txt | awk '{ if ($10 + 0 > 90) exit 0; else { print "Line coverage is " $10 "% (< 90%)"; exit 1 } }'
+          '';
+          installPhase = ''
+            mkdir -p $out
+            if [ -f "build/coverage.txt" ]; then
+              cp build/coverage.txt $out/
+            fi
+            if [ -d "build/coverage" ]; then
+              cp -R build/coverage $out/html
+            fi
+          '';
+        };
+      });
+      devShells = forAllSystems (pkgs: {
+        default =
+          pkgs.mkShell.override
+            {
+              stdenv = pkgs.clangStdenv; # Clang instead of GCC
+            }
+            {
+              packages = with pkgs; [
+                bashInteractive
+                clang-tools # Clang CLIs, including LSP
+                clang-uml # UML diagram generator
+                cmake-format # CMake formatter
+                cmake-language-server # Cmake LSP
+                # cppcheck # C++ Static analysis
+                doxygen # Documentation generator
+                # fluidsynth # JACK Synthesizer
+                lldb # Clang debug adapter
+                # neocmakelsp # CMake LSP
+                # qsynth # FluidSynth GUI
+                socat # Serial terminal for manual testing
+                # valgrind # Debugging and profiling
+              ];
+              nativeBuildInputs = self.packages.${pkgs.stdenv.hostPlatform.system}.smart-piano.nativeBuildInputs;
+              buildInputs = self.packages.${pkgs.stdenv.hostPlatform.system}.smart-piano.buildInputs;
+              # Export compile commands JSON for LSP and other tools
+              shellHook = ''
+                mkdir --verbose build
+                cmake -DCMAKE_BUILD_TYPE=Debug -DCOVERAGE=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -S . -B build
+              '';
+            };
+      });
     };
 }
